31508c992ed70fc7cf3178e6cbb6720edddc2765
Yang <627756022@qq.com>
2017-12-15 19:23:59 +0800

    《毕业设计——个人摄影网站》 ------第一版上传------ ----首页搭建完成 ----2017-12-14（0.3）

--- src/main/webapp/static/myjs/ygblog-js.js
+++ src/main/webapp/static/myjs/ygblog-js.js
@@ -7,3 +7,12 @@ function run() {
         }
     });
 }
+function Loding() {
+    //如果点击加载更多，则执行这个函数，加载完后文本重置。
+    // var a_imgloding = $('#a_imgLonding');
+    // a_imgloding.innerHTML = '加载中...';
+    $('#a_imgLonding').text("加载中...");
+    setInterval(function () {
+        $('#a_imgLonding').text("加载更多");
+    },2000)
+}
